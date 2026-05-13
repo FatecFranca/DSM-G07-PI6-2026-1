fbeba0cdb1816249bb6ad8e3ca15c28d797d0e0a
Gabriel Resende Spirlandelli <grspirlandelli@gmail.com>
2026-05-12 22:08:49 -0300

    fix pipeline

--- api-java/src/main/java/com/petdex/api/application/services/localizacao/ImplLocalizacaoService.java
+++ api-java/src/main/java/com/petdex/api/application/services/localizacao/ImplLocalizacaoService.java
@@ -14,6 +14,7 @@ import com.petdex.api.infrastructure.mongodb.LocalizacaoRepository;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
