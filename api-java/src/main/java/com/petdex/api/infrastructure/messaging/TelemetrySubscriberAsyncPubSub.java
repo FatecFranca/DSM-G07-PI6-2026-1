c13a2f5aa92bfcbeb1956b8fe8b090f523e1dcc9
Gabriel Resende Spirlandelli <grspirlandelli@gmail.com>
2026-06-04 21:00:58 -0300

    fix: PubSub não dava nack nas mensagens

--- api-java/src/main/java/com/petdex/api/infrastructure/messaging/TelemetrySubscriberAsyncPubSub.java
+++ api-java/src/main/java/com/petdex/api/infrastructure/messaging/TelemetrySubscriberAsyncPubSub.java
@@ -1,10 +1,6 @@
 package com.petdex.api.infrastructure.messaging;
 
-import com.google.cloud.pubsub.v1.AckReplyConsumer;
-import com.google.cloud.pubsub.v1.MessageReceiver;
-import com.google.cloud.pubsub.v1.Subscriber;
-import com.google.pubsub.v1.ProjectSubscriptionName;
-import com.google.pubsub.v1.PubsubMessage;
+import com.google.cloud.spring.pubsub.core.PubSubTemplate;
 import com.petdex.api.application.services.messaging.subscriber.TelemetrySubscriberService;
 import com.petdex.api.infrastructure.messaging.contracts.TelemetrySubscriberAsync;
 import org.slf4j.Logger;
@@ -14,7 +10,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-
 @Component
 public class TelemetrySubscriberAsyncPubSub implements CommandLineRunner, TelemetrySubscriberAsync {
 
@@ -22,10 +17,12 @@ public class TelemetrySubscriberAsyncPubSub implements CommandLineRunner, Teleme
     String projectId;
 
     @Autowired
+    private PubSubTemplate pubSubTemplate;
+
+    @Autowired
     private TelemetrySubscriberService telemetrySubscriberService;
 
     private Logger logger = LoggerFactory.getLogger(TelemetrySubscriberAsyncPubSub.class);
-
 
     public void run(String... args) throws Exception {
         String subscriptionId = "petdex-telemetry-sub";
@@ -33,42 +30,29 @@ public class TelemetrySubscriberAsyncPubSub implements CommandLineRunner, Teleme
     }
 
     @Override
-     public void subscribeAsync(String projectId, String subscriptionId) {
-        ProjectSubscriptionName subscriptionName = ProjectSubscriptionName.of(projectId, subscriptionId);
-
-        MessageReceiver receiver = (PubsubMessage message, AckReplyConsumer consumer) -> {
-            try {
-                logger.info("Mensagem recebida. ID: {}", message.getMessageId());
-                String data = message.getData().toStringUtf8();
-                if(!telemetrySubscriberService.processarMensagem(data)) {
-                    logger.error("Erro ao processar a mensagem. ID: {}", message.getMessageId());
-                    consumer.nack();
-                } else {
-                    logger.info("Mensagem processada com sucesso. ID: {}", message.getMessageId());
-                    consumer.ack();
-                }
-            } catch (Exception e) {
-                logger.error("Exceção ao processar mensagem ID: {}. Erro: {}", message.getMessageId(), e.getMessage());
-                consumer.nack();
-            }
-        };
-
-        Subscriber subscriber = null;
+    public void subscribeAsync(String projectId, String subscriptionId) {
         try {
-            subscriber = Subscriber.newBuilder(subscriptionName, receiver).build();
-            subscriber.startAsync().awaitRunning();
-            logger.info("Subscription iniciada: {}", subscriptionName);
-            try {
-                Thread.currentThread().join();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                logger.error("Thread interrompido: {}", e.getMessage());
-            }
+            logger.info("Iniciando escuta assíncrona da subscrição: {}", subscriptionId);
+            pubSubTemplate.subscribe(subscriptionId, message -> {
+                String messageId = message.getPubsubMessage().getMessageId();
+                try {
+                    logger.info("Mensagem recebida. ID: {}", messageId);
+                    String data = message.getPubsubMessage().getData().toStringUtf8();
+                    if (!telemetrySubscriberService.processarMensagem(data)) {
+                        logger.error("Erro ao processar a mensagem. ID: {}", messageId);
+                        message.nack();
+                    } else {
+                        logger.info("Mensagem processada com sucesso. ID: {}", messageId);
+                        message.ack();
+                    }
+                } catch (Exception e) {
+                    logger.error("Exceção ao processar mensagem ID: {}. Erro: {}", messageId, e.getMessage());
+                    message.nack();
+                }
+            });
+            logger.info("Subscription iniciada com sucesso para: {}", subscriptionId);
         } catch (Exception exception) {
-            logger.error("Erro: {}", exception.getMessage());
-            if (subscriber != null) {
-                subscriber.stopAsync();
-            }
+            logger.error("Erro ao iniciar subscrição {}: {}", subscriptionId, exception.getMessage());
         }
     }
 }
